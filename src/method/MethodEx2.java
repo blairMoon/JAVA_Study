c6cd8018f8c8a3a3e6991905db8b1d2f1a92b42b
blairMoon <catherine621@naver.com>
2024-01-26 13:04:38 +0900

    MedthodEx2

--- src/method/MethodEx2.java
+++ src/method/MethodEx2.java
@@ -1,2 +1,30 @@
-package method;public class MethodEx2 {
+package method;
+
+public class MethodEx2 {
+
+    public static void main(String[] args) {
+        String message = "Hello, world!";
+        printHelloWorld(message,3);
+        System.out.println("___________");
+
+        printHelloWorld(message, 5);
+        System.out.println("___________");
+        printHelloWorld(message,7);
+
+
+
+
+        }
+    public static void printHelloWorld(String message , int num){
+
+
+        for (int i = 0; i < num ; i++){
+            System.out.println(message);
+
+        }
+
+
+
+
+    }
 }
